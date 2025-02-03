c2f2e711b747fc5f405ebf479f6ad2c6b2932ce5
Nemanja Porobic <n.porobic@vegait.rs>
2025-02-03 20:56:27 +0100

    Add heap

---
+++ ds_and_algorithms_for_beginners/08_heap_and_priority_queue/heap.py
@@ -0,0 +1,88 @@
+# Priority queues, implemented by binary heap, (priority based on min or max values)
+# 1.) Structure property - we want to have the complete tree and add the property
+# until we fill out the whole row
+# 2.) Order property - we want to define if we want to prioritize based on min or max value
+# every value of left and every value of right needs to be greater than or equal (>=) root node, it's okay
+
+# Note: We use arrays instead of nodes and pointers because we need to work with
+# complete trees and is much easier
+
+# Min Heap
+class Heap:
+    def __init__(self):
+        self.heap = [0]
+
+    def push(self, val):
+        self.heap.append(val)
+        i = len(self.heap) - 1  # find last index
+
+        # Percolate up
+        while i > 1 and self.heap[i] < self.heap[i // 2]:
+            tmp = self.heap[i]
+            self.heap[i] = self.heap[i // 2]
+            self.heap[i // 2] = tmp
+            i = i // 2
+
+    def pop(self):
+        if len(self.heap) == 1:
+            return None
+        if len(self.heap) == 2:
+            return self.heap.pop()
+
+        res = self.heap[1]
+        # Move last value to root
+        self.heap[1] = self.heap.pop()
+        i = 1
+        # Percolate down
+        while 2 * i < len(self.heap):
+            if (2 * i + 1 < len(self.heap) and
+            self.heap[2 * i + 1] < self.heap[2 * i] and
+            self.heap[i] > self.heap[2 * i + 1]):
+                # Swap right child
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[2 * i + 1]
+                self.heap[2 * i + 1] = tmp
+                i = 2 * i + 1
+            elif self.heap[i] > self.heap[2 * i]:
+                # Swap left child
+                tmp = self.heap[i]
+                self.heap[i] = self.heap[2 * i]
+                self.heap[2 * i] = tmp
+                i = 2 * i
+            else:
+                break
+        return res
+
+    def top(self):
+        if len(self.heap) > 1:
+            return self.heap[1]
+        return None
+
+    def heapify(self, arr):
+        # 0-th position is moved to the end
+        arr.append(arr[0])
+
+        self.heap = arr
+        cur = (len(self.heap) - 1) // 2
+        while cur > 0:
+            # Percolate down
+            i = cur
+            while 2 * i < len(self.heap):
+                if (2 * i + 1 < len(self.heap) and
+                self.heap[2 * i + 1] < self.heap[2 * i] and
+                self.heap[i] > self.heap[2 * i + 1]):
+                    # Swap right child
+                    tmp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i + 1]
+                    self.heap[2 * i + 1] = tmp
+                    i = 2 * i + 1
+                elif self.heap[i] > self.heap[2 * i]:
+                    # Swap left child
+                    tmp = self.heap[i]
+                    self.heap[i] = self.heap[2 * i]
+                    self.heap[2 * i] = tmp
+                    i = 2 * i
+                else:
+                    break
+            cur -= 1
+
