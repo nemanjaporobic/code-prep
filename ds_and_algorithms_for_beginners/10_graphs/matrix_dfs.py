1c7220a8a49bd81aa9293523cc905476c5f9c2f1
Nemanja Porobic <n.porobic@vegait.rs>
2025-02-10 10:13:53 +0100

    Add hasihng and graphs data structures

---
+++ ds_and_algorithms_for_beginners/10_graphs/matrix_dfs.py
@@ -0,0 +1,35 @@
+# Matrix (2D Grid)
+grid = [[0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0]]
+
+
+# Question: Count the uniqu paths from the top left to the
+# bottom right. A single path may only move along 0's and
+# can't visit the same cell more than once.
+# Count paths (backtracking)
+def dfs(grid, r, c, visit):
+    ROWS, COLS = len(grid), len(grid[0])
+    if (min(r, c) < 0 or
+        r == ROWS or c == COLS or
+        (r, c) in visit or grid[r][c] == 1):
+        return 0
+    if r == ROWS - 1 and c == COLS - 1:
+        return 1
+
+    visit.add((r, c))
+
+    count = 0
+    count += dfs(grid, r + 1, c, visit)
+    count += dfs(grid, r - 1, c, visit)
+    count += dfs(grid, r, c + 1, visit)
+    count += dfs(grid, r, c - 1, visit)
+
+    visit.remove((r, c))
+    return count
+
+
+if __name__ == '__main__':
+    print(f'Total count of unique paths is: {dfs(grid, 0, 0, set())}')
+
